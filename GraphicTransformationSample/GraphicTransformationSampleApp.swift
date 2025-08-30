7f48f5023e29ce38d67e6a9ad838a0f06c2a6345
kim-sungyeon <sykim8770@gmail.com>
2025-08-30 13:16:23 +0900

    Initial Commit

---
+++ GraphicTransformationSample/GraphicTransformationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphicTransformationSampleApp.swift
+//  GraphicTransformationSample
+//
+//  Created by 김성연 on 8/30/25.
+//
+
+import SwiftUI
+
+@main
+struct GraphicTransformationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
